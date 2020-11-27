77926262f36a6c3cb61f53ba562e55a993bd11e4
CollinAvidano <collin.avidano@gmail.com>
2020-11-27 17:02:12 -0500

    touchpad searches for id and added i3 lock start line to i3 config

--- i3/.config/i3/toggletouchpad.sh
+++ i3/.config/i3/toggletouchpad.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-device=$1
-state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
+device_name='Synaptics TM3289-021'
+device_id=$(xinput list --id-only "$device_name")
+
+state=$(xinput list-props "$device_id" | grep "Device Enabled" | grep -o "[01]$")
 
 if [ $state == '1' ];then
-  xinput --disable "$device"
+  xinput --disable "$device_id"
 else
-  xinput --enable "$device"
+  xinput --enable "$device_id"
 fi
